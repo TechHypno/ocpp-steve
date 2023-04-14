5081780d65c2f42c10b4d4717fc37e3c55c48182
TechHypno <techhypno.com@gmail.com>
2023-04-14 16:49:09 +0200

    set config values

---
+++ config-setup.sh
@@ -0,0 +1,6 @@
+sed -i -e '/db\.password =/ s/= *.*/= '"$MYSQL_PASSWORD"'/
+        /auth\.user =/ s/= *.*/= '"$OCPP_ADMIN_USER"'/
+        /auth\.password =/ s/= *.*/= '"$OCPP_ADMIN_PASSWORD"'/
+        /webapi\.key =/ s/= *.*/= '"$OCPP_WEBAPI_KEY"'/
+        /webapi\.value =/ s/= *.*/= '"$OCPP_WEBAPI_VALUE"'/
+        /server\.host =/ s/= *.*/= '"$OCPP_SERVER_IP"'/' src/main/resources/config/docker/main.properties
